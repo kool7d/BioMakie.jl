5b64fd3745f32533d9d8c94b98696e8785121416
Daniel Kool <kool7d@gmail.com>
2020-08-23 22:59:48 -0500

    aea

--- docs/make.jl
+++ docs/make.jl
@@ -7,14 +7,16 @@ using AbstractPlotting, Makie, MakieLayout
 makedocs(
     sitename="BioMakie",
     format=Documenter.HTML(),
-    pages = Any[
-                "index.md",
-                "Examples" => "literate/examples.md",
-               ],
+    pages = [
+                "Home" => "index.md"
+            ],
     repo="https://github.com/kool7d/BioMakie.jl.git"
 )
 
 deploydocs(
-    repo="github.com/kool7d/BioMakie.jl.git"
+    repo="github.com/kool7d/BioMakie.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing
 )
-# include("make.jl")
+include("make.jl")
