056bf3b88717200d9c4ea7b79f9c157107a09e6d
Daniel Kool <kool7d@gmail.com>
2020-10-09 22:12:22 -0500

    ao

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Pkg
+Pkg.add("Documenter")
+using Documenter, BioMakie
+
+makedocs(
+    sitename="BioMakie",
+    modules = [BioMakie],
+    format = Documenter.HTML(),
+    pages = ["Home" => "index.md",
+            "Page1" => "page1.md"])
+
+deploydocs(repo = "github.com/kool7d/BioMakie",
+    branch = "gh-pages",
+    target = "docs")
