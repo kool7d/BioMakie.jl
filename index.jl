36d659ac3ac8089cfb956096deb341614bd6ae37
kool7d <kool7d@gmail.com>
2020-10-12 11:59:50 +0000

    Deploying to gh-pages from  @ 14034c424ccb6b12e3e342dbacd68f41ef09f05a 🚀

--- index.jl
+++ index.jl
@@ -1,11 +1,11 @@
 #md # ```@meta
 #md # CurrentModule = BioMakie
 #md # ```
-# # # BioMakie
+# # BioMakie
 
 # A user interface for bioinformatics.
 
-# # Description
+# ## Description
 
 # BioMakie.jl is a package designed to facilitate visual investigation of biological
 # data. It aims to provide extra tools to view and measure differences between data
@@ -23,7 +23,7 @@
 # For more examples of what Makie can do, visit the documentation at
 # https://makie.juliaplots.org/stable/
 
-# # Setup
+# ## Setup
 
 ## in the REPL
 ]add BioMakie
@@ -35,27 +35,16 @@ Pkg.add("BioMakie")
 ## import the package
 using BioMakie
 
-# # Basic Usage
+# ## Basic Usage
 
 # To view a PDB structure, use the `viewstruc` function with a PDB ID. The following code plots the structure then returns a StructureView with the scene and layout.
 
 sv = viewstruc("2VB1")
-#md # ```@example
-#md # https://github.com/kool7d/BioMakie.jl/blob/master/assets/2vb1.png
-#md # ```
+#md # ![Image of struc](https://github.com/kool7d/BioMakie.jl/blob/master/assets/2vb1.png)
 
 # You could also look at multiple structures at once.
 svs = viewstrucs(["2vb1","1lw3"], colors = ["aqua","ele"])
-display(svs.scenes[1])
-display("https://github.com/kool7d/BioMakie.jl/blob/master/assets/pf00062.png")
-show("https://github.com/kool7d/BioMakie.jl/blob/master/assets/2strucs.png")
-AbstractPlotting
-using CairoMakie
-To view a multiple sequence alignment, use the `viewmsa` function with a Pfam ID (fasta coming soon)
-mv = viewmsa("PF00062")
-# Literate.markdown("src/index.jl","src")
-# [docs-img]: https://github.com/kool7d/BioMakie.jl/blob/master/assets/pf00062.png
-import DisplayAs
-img = plot(...)
-img = DisplayAs.PNG("https://github.com/kool7d/BioMakie.jl/blob/master/assets/2strucs.png")
-plot(img)
+#md # ![Image of strucs](https://github.com/kool7d/BioMakie.jl/blob/master/assets/2strucs.png)
+
+using Literate #src
+Literate.markdown("src/index.jl","src") #src
