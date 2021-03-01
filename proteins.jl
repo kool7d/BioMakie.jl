b7559ee2306c3ad4c3b95022b4d3478173a7665c
kool7d <kool7d@gmail.com>
2021-03-01 18:35:15 +0000

    Deploying to gh-pages from @ kool7d/BioMakie.jl@8684e552d3a4e4c84d6263e4d292500e4ca85ae6 🚀

--- proteins.jl
+++ proteins.jl
@@ -1,23 +1,24 @@
-# # Proteins
+# Proteins
 
-# ## Structures
+## Structures
 
-# You can load a Protein Structure from the PDB (Protein Data Bank) using its ID like so:
+You can load a Protein Structure from the PDB (Protein Data Bank):
+
+```@example proteins
 pstruc1 = viewstruc("2VB1")
+```
 
-# You could also look at multiple structures at once.
-svs = viewstrucs(["2vb1","1lw3"])
-#md # ![](https://github.com/kool7d/BioMakie.jl/blob/master/assets/2vb1.png)
-#md # [![](https://github.com/kool7d/BioMakie.jl/blob/master/assets/2vb1.png)]
-# svs.scene[1]
-# ## Multiple sequence alignments
+## Multiple sequence alignments
 
-# You can similarly load a Multiple Sequence Alignment from the Pfam database using its
-# ID like so:
-# msa1 = viewmsa("PF00062")
+You can similarly load a Multiple Sequence Alignment from the Pfam database:
+```@example proteins
+msa1 = viewmsa("PF00062")
+```
 
 #md # ![](msa1.svg)
 #md # ![Image of msa](pf00062.png)
+# An `MSAView` contains the most relevant information about the protein sequences in the
+# alignment along with the `Scene` and `Layout` which comprise the interface.
 
 # using Literate #src
 # Literate.markdown("src/proteins.jl","src") #src
